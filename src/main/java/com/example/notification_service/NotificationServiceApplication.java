20ed34060e0c5bc53f61883899e63cd724c708d3
Ismail Ourakh <135983685+ismailourakh@users.noreply.github.com>
2025-01-07 05:13:24 +0100

    email marche

--- src/main/java/com/example/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/example/notification_service/NotificationServiceApplication.java
@@ -22,4 +22,14 @@ public class NotificationServiceApplication implements CommandLineRunner {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Sending an email example
+		String recipient = "meftahahmedreda02@gmail.com";
+		String subject = "Test Email";
+		String message = "This is a test email from the Notification Service.";
+
+		System.out.println("Envoi de l'e-mail...");
+		emailService.sendEmail(recipient, subject, message);
+	}
 }
