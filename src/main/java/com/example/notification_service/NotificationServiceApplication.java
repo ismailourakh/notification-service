01d09134eef67eb6fffae0c43e2af4a35ef80166
Ismail Ourakh <135983685+ismailourakh@users.noreply.github.com>
2025-01-07 05:14:37 +0100

    email marche3

--- src/main/java/com/example/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/example/notification_service/NotificationServiceApplication.java
@@ -1,7 +1,5 @@
 package com.example.notification_service;
 
-import com.example.notification_service.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,25 +9,12 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("file:.env")
 public class NotificationServiceApplication implements CommandLineRunner {
 
-	private final EmailService emailService;
-
-	@Autowired
-	public NotificationServiceApplication(EmailService emailService) {
-		this.emailService = emailService;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// Sending an email example
-		String recipient = "meftahahmedreda02@gmail.com";
-		String subject = "Test Email";
-		String message = "This is a test email from the Notification Service.";
-
-		System.out.println("Envoi de l'e-mail...");
-		emailService.sendEmail(recipient, subject, message);
+		System.out.println("Notification Service démarré avec succès !");
 	}
 }
